21105365475d3059a9562337a72838d83bfeb62b
HeyTech <mujtaba.aldebes@hotmail.com>
2018-07-06 15:49:05 +0200

    update client

---
+++ MainUno.py
@@ -0,0 +1,15 @@
+import client
+
+
+def start_game():
+    host = input('Enter host ip:')
+    player_name = input('Enter you username:')
+
+    print(host, player_name)
+    client.login_to_game(host, player_name)
+
+
+
+
+if __name__ == '__main__':
+    start_game()
--- client.py
+++ client.py
@@ -1,45 +1,41 @@
 #!/usr/bin/env python
-# Socket client example in python
-
 import socket
 import sys
 
-host = '192.168.233.164'
-port = 4444  # web
-Player_name = input("Who are you: ")
-Player_name += " / HTTP/1.0\r\n\r\n"
-# create socket
-print('# Creating socket')
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-except socket.error:
-    print('Failed to create socket')
-    sys.exit()
+def login_to_game(host, player_name):
+    port = 4444  # web
 
+    player_name += " / HTTP/1.0\r\n\r\n"
+    # create socket
+    print('# Creating socket')
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error:
+        print('Failed to create socket')
+        sys.exit()
 
-print('# Getting remote IP address')
-try:
-    remote_ip = socket.gethostbyname( host )
-except socket.gaierror:
-    print('Hostname could not be resolved. Exiting')
-    sys.exit()
+    print('# Getting remote IP address')
+    try:
+        remote_ip = socket.gethostbyname(host)
+    except socket.gaierror:
+        print('Hostname could not be resolved. Exiting')
+        sys.exit()
 
-# Connect to remote server
-print('# Connecting to server, ' + host + ' (' + remote_ip + ')')
-s.connect((remote_ip , port))
+    # Connect to remote server
+    print('# Connecting to server, ' + host + ' (' + remote_ip + ')')
+    s.connect((remote_ip, port))
 
-# Send data to remote server
-print('# Sending data to server')
+    # Send data to remote server
+    print('# Sending data to server')
 
-try:
-    s.sendall(Player_name.encode())
-except socket.error:
-    print('Send failed')
-    sys.exit()
+    try:
+        s.sendall(player_name.encode())
+    except socket.error:
+        print('Send failed')
+        sys.exit()
 
-# Receive data
-print('# Receive data from server')
-reply = s.recv(4096)
+    # Receive data
+    print('# Receive data from server')
+    reply = s.recv(4096)
 
-print(reply)
-
+    print(reply)
